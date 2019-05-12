a1c1c2e41ed6ec8b35017fb60cb5f7efc1dba87c
lapiscine <jaime.sastre@lapiscine.pro>
2019-05-13 00:44:31 +0200

    Test page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,25 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('@App/index.html.twig', [
-            'test' => 'test'
+            'test' => 'test',
         ]);
     }
+
+
+    /**
+     *
+     * @Route("/bundle", name="bundle")
+     */
+    public function bundleAction(){
+
+        return $this-> render( '@App/pages/bundle.html.twig' , [
+                'auteur' => 'Jaime Sastre',
+                'titre' => 'Article parlant de Symfony',
+                'contenu' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium cum dolor earum, exercitationem expedita fugiat illo inventore ipsa itaque minima nam odio omnis perferendis porro quia, quod ullam, unde?',
+            ]
+        );
+    }
+
+
+
 }
